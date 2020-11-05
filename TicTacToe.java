20e9a371db72c5bc0e7b0a7a2cf6c7d5e9963a65
ManaliParab12 <manaliparab10@gmail.com>
2020-11-05 17:47:50 +0530

    Added Initialize Board method to make board empty

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,11 +3,19 @@ package tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-	static Scanner sc = new Scanner(System.in);
+	
 	static char[]board;
 	
 	public static void main(String []args) {
 		
 		System.out.println("tic tac toe Game");	
+		initializeBoard();
+	}
+	
+	static void initializeBoard() {
+	    board = new char[10]; 
+		for(int i = 1; i< 10; i++){
+			board[i]='-';
+		}
 	}
 }
