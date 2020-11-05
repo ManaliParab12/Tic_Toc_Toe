c2bf60c2ee21cdf1c98cb663f600bdf540e8965e
ManaliParab12 <manaliparab10@gmail.com>
2020-11-05 17:56:17 +0530

    Added Input method to take X or O from user

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	
+	static Scanner sc = new Scanner(System.in);
 	static char[]board;
 	
 	public static void main(String []args) {
 		
-		System.out.println("tic tac toe Game");	
+		System.out.println("Welcome To Tic-Tac-Toe Game");	
 		initializeBoard();
+		input();
 	}
 	
 	static void initializeBoard() {
@@ -18,4 +20,21 @@ public class TicTacToe {
 			board[i]='-';
 		}
 	}
+	
+	static void input() {
+		
+		System.out.println("Enter option X or O");
+		char input = sc.next().toUpperCase().charAt(0);
+		String compute;
+		if(input == 'X') {
+			compute="o";
+		}
+		else if(input =='O') {
+			compute="x";
+		}
+		else {
+			System.out.println("Enter correct choice");
+			input();
+		}	
+	}	
 }
